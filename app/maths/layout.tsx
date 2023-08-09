e76db885ea3fc11895446a3133c49d7f61bb0338
Brian Cao <chasoft@gmail.com>
2023-08-09 11:09:45 +0700

    update layout

--- app/maths/layout.tsx
+++ app/maths/layout.tsx
@@ -1,17 +1,11 @@
-export default function RandomLayout({
-	children,
-	details,
-	settings,
+export default function MathsLayout({
+	children
 }: {
 	children: React.ReactNode
-	details: React.ReactNode
-	settings: React.ReactNode
 }) {
 	return (
 		<div className="w-full h-full px-3 py-20 overflow-hidden md:py-28 md:px-6">
 			{children}
-			{details}
-			{settings}
 		</div>
 	)
 }
